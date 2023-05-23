d702b6b92c235eec0ce988e00411908afdab85b6
Keln <122953267+EgorKeln@users.noreply.github.com>
2023-05-23 23:35:09 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+AOS.init();
+
+let menuToggle = document.querySelector('.menuToggle');
+let close0 = document.querySelector('.close0');
+let close1 = document.querySelector('.close1');
+let close2 = document.querySelector('.close2');
+let close3 = document.querySelector('.close3');
+let close4 = document.querySelector('.close4');
+let close5 = document.querySelector('.close5');
+let header = document.querySelector('header');
+
+    menuToggle.onclick = function(){
+        header.classList.toggle('active')
+    }
+    close0.onclick = function(){
+        header.classList.toggle('active')
+    }
+    close1.onclick = function(){
+        header.classList.toggle('active')
+    }
+    close2.onclick = function(){
+        header.classList.toggle('active')
+    }
+    close3.onclick = function(){
+        header.classList.toggle('active')
+    }
+    close4.onclick = function(){
+        header.classList.toggle('active')
+    }
+    close5.onclick = function(){
+        header.classList.toggle('active')
+    }
