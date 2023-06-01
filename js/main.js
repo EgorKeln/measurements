6a3bef2e8dad9cc31654b4e84e4b7ad369f51850
Keln <122953267+EgorKeln@users.noreply.github.com>
2023-06-01 03:42:07 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,33 +1,78 @@
 
 AOS.init();
 
-let menuToggle = document.querySelector('.menuToggle');
+const hamburger = document.querySelector('.hamburger');
+const navigation = document.querySelector('.navigation');
+
+hamburger.addEventListener('click', () => {
+	navigation.classList.toggle('is-open');
+	hamburger.classList.toggle('is-open');
+
+})
+
 let close0 = document.querySelector('.close0');
 let close1 = document.querySelector('.close1');
 let close2 = document.querySelector('.close2');
 let close3 = document.querySelector('.close3');
 let close4 = document.querySelector('.close4');
 let close5 = document.querySelector('.close5');
-let header = document.querySelector('header');
 
-    menuToggle.onclick = function(){
-        header.classList.toggle('active')
-    }
-    close0.onclick = function(){
-        header.classList.toggle('active')
-    }
-    close1.onclick = function(){
-        header.classList.toggle('active')
-    }
-    close2.onclick = function(){
-        header.classList.toggle('active')
-    }
-    close3.onclick = function(){
-        header.classList.toggle('active')
-    }
-    close4.onclick = function(){
-        header.classList.toggle('active')
-    }
-    close5.onclick = function(){
-        header.classList.toggle('active')
-    }
+close0.onclick = function(){
+	navigation.classList.toggle('is-open');
+	hamburger.classList.toggle('is-open');
+}
+close1.onclick = function(){
+	navigation.classList.toggle('is-open');
+	hamburger.classList.toggle('is-open');
+}
+close2.onclick = function(){
+	navigation.classList.toggle('is-open');
+	hamburger.classList.toggle('is-open');
+}
+close3.onclick = function(){
+	navigation.classList.toggle('is-open');
+	hamburger.classList.toggle('is-open');
+}
+close4.onclick = function(){
+	navigation.classList.toggle('is-open');
+	hamburger.classList.toggle('is-open');
+}
+close5.onclick = function(){
+	navigation.classList.toggle('is-open');
+	hamburger.classList.toggle('is-open');
+}
+
+let cardOnClickOne = document.querySelector('.cardOnClickOne');
+let OpenOne = document.querySelector('.OpenOne');
+let CloseOne = document.querySelector('.CloseOne');
+let cardOnClickTwo = document.querySelector('.cardOnClickTwo');
+let OpenTwo = document.querySelector('.OpenTwo');
+let CloseTwo = document.querySelector('.CloseTwo');
+let cardOnClickThree = document.querySelector('.cardOnClickThree');
+let OpenThree = document.querySelector('.OpenThree');
+let CloseThree = document.querySelector('.CloseThree');
+OpenOne.onclick = function(){
+    cardOnClickOne.classList.add('activeСardOne');
+    cardOnClickTwo.classList.remove('activeСardTwo');
+    cardOnClickThree.classList.remove('activeСardThree');
+}
+OpenTwo.onclick = function(){
+    cardOnClickTwo.classList.add('activeСardTwo');
+    cardOnClickOne.classList.remove('activeСardOne');
+    cardOnClickThree.classList.remove('activeСardThree');
+}
+OpenThree.onclick = function(){
+    cardOnClickThree.classList.add('activeСardThree');
+    cardOnClickOne.classList.remove('activeСardOne');
+    cardOnClickTwo.classList.remove('activeСardTwo');
+}
+CloseOne.onclick = function(){
+    cardOnClickOne.classList.remove('activeСardOne');
+}
+CloseTwo.onclick = function(){
+    cardOnClickTwo.classList.remove('activeСardTwo');
+}
+CloseThree.onclick = function(){
+    cardOnClickThree.classList.remove('activeСardThree');
+}
+
